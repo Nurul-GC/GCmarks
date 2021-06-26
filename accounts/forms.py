b312e2ad957c034bec0b33854357258fe173dd21
Nurul-GC <nuruldecarvalho@gmail.com>
2021-06-26 03:19:12 +0200

    update commit

---
+++ accounts/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password', widget=forms.PasswordInput({'placeholder': 'Type your password'}))
+    password2 = forms.CharField(label='Retype password', widget=forms.PasswordInput({'placeholder': 'Retype your password'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("Passwords don't match.")
+        return cd['password2']
+
+
+class LoginForm(forms.Form):
+    name = forms.CharField(widget=forms.TextInput({'placeholder': 'Type your name'}))
+    password = forms.CharField(widget=forms.PasswordInput({'placeholder': 'Type your password'}))
