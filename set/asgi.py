364b7ec0b28c542599230e32a6ac4fb8e32e1f81
Nurul-GC <nuruldecarvalho@gmail.com>
2021-06-26 03:17:47 +0200

    update commit

--- set/asgi.py
+++ set/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for GCmarks project.
+ASGI config for set project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GCmarks.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'set.settings')
 
 application = get_asgi_application()
