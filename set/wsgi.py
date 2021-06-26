ffdff40b7626a2a81830d24652cc90e4e9c56946
Nurul-GC <nuruldecarvalho@gmail.com>
2021-06-26 03:17:59 +0200

    update commit

--- set/wsgi.py
+++ set/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for GCmarks project.
+WSGI config for set project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GCmarks.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'set.settings')
 
 application = get_wsgi_application()
