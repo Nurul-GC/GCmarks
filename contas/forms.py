5c797886391767fc2cead0b0d0ba7b8b417c04a9
Nurul-GC <nuruldecarvalho@gmail.com>
2021-06-26 03:18:35 +0200

    update commit

--- contas/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class LoginForm(forms.Form):
-    nome = forms.CharField(widget=forms.TextInput({'placeholder': 'Digite o seu nome'}))
-    senha = forms.CharField(widget=forms.PasswordInput({'placeholder': 'Digite a sua senha'}))
