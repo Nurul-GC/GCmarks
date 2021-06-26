afcd4490363d04acad675529eec02a525d973fbf
Nurul-GC <nuruldecarvalho@gmail.com>
2021-06-26 03:17:33 +0200

    update commit

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GCmarks.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'set.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
