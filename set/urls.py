0a0ebc7ce49f3f536526d6e69e3c073a90f091b3
Nurul-GC <nuruldecarvalho@gmail.com>
2021-06-26 03:17:55 +0200

    update commit

--- set/urls.py
+++ set/urls.py
@@ -1,4 +1,4 @@
-"""GCmarks URL Configuration
+"""set URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from contas import views
+from accounts import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inicio/', include('contas.urls')),
+    path('', include('accounts.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
