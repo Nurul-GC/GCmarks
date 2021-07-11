220a7231b9d6c537511113f9c7c68729bbe620ca
Nurul-GC <nuruldecarvalho@gmail.com>
2021-07-11 16:10:22 +0200

    update commit

---
+++ images/views.py
@@ -0,0 +1,21 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+from images.forms import ImageCreateForm
+
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_item = form.save(commit=False)
+            new_item.user = request.user
+            new_item.save()
+            messages.success(request, 'Image added successfully')
+            return redirect(new_item.get_absolute_url())
+    else:
+        form = ImageCreateForm(data=request.GET)
+    return render(request, 'create.html', {'section': 'images', 'form': form})
